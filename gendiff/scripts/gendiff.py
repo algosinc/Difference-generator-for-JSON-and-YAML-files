bd806d780b718f21d840f557142ea9b09564727d
Ivan Aleksashin <algos.inc@gmail.com>
2021-11-12 01:56:14 +0100

    update json comparator

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,7 +4,7 @@ import json
 import argparse
 
 
-def compare(first_json, second_json):
+def json_compare(first_json, second_json):
     keys = set(first_json.keys())
     keys_list = list(keys.union(set(second_json.keys())))
     keys_list.sort()
@@ -12,22 +12,22 @@ def compare(first_json, second_json):
     for key in keys_list:
         if (key in first_json) and (key in second_json):
             if first_json[key] == second_json[key]:             # no changes
-                result.append(f'    {encode(key)}: {encode(first_json[key])}')
+                result.append(f'    {json_encode(key)}: {json_encode(first_json[key])}')
             if first_json[key] != second_json[key]:             # value changed
-                result.append(f'  - {encode(key)}: {encode(first_json[key])}')
-                result.append(f'  + {encode(key)}: {encode(second_json[key])}')
+                result.append(f'  - {json_encode(key)}: {json_encode(first_json[key])}')
+                result.append(f'  + {json_encode(key)}: {json_encode(second_json[key])}')
             continue
         if (key in first_json) and not (key in second_json):
-            result.append(f'  - {encode(key)}: {encode(first_json[key])}')  # noqa # remove item
+            result.append(f'  - {json_encode(key)}: {json_encode(first_json[key])}')  # noqa # remove item
             continue
         else:
-            result.append(f'  + {encode(key)}: {encode(second_json[key])}')  # noqa # new item
+            result.append(f'  + {json_encode(key)}: {json_encode(second_json[key])}')  # noqa # new item
     result.append('}')
     return '\n'.join(result)
 
 
-def encode(value_in_json):
-    return json.JSONEncoder().encode(value_in_json)
+def json_encode(value_in_json):
+    return json.JSONEncoder().json_encode(value_in_json)
 
 
 def generate_diff(first_file, second_file, format):
@@ -35,7 +35,7 @@ def generate_diff(first_file, second_file, format):
         with open(second_file) as f2:
             first_json = json.load(f1)
             second_json = json.load(f2)
-            return compare(first_json, second_json)
+            return json_compare(first_json, second_json)
 
 
 def main():
