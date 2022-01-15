5815bff235b03fb34d48423725c855d2a08fd17d
algos <algos.inc@gmail.com>
2022-01-15 17:31:22 +0100

    refactor CLI: add constants use

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import argparse
+from gendiff.constants import STYLISH, PLAIN, JSON, DEFAULT_FORMAT
 
 
 def cli():
@@ -9,8 +10,8 @@ def cli():
     parser.add_argument('second_file', metavar='second_file', type=str)
     parser.add_argument('-f',
                         '--format',
-                        dest='format',
-                        default='json',
+                        choices=[STYLISH, PLAIN, JSON],
+                        default=DEFAULT_FORMAT,
                         help='set format of output (default: JSON)')
     args = parser.parse_args()
     return args.first_file, args.second_file, args.format
