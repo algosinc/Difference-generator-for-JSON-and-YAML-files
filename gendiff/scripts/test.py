c13f31f1f9e25ce17da8e7b7869a1d96c8a59388
algos <algos.inc@gmail.com>
2022-01-16 02:25:48 +0100

    remove dev files

--- gendiff/scripts/test.py
+++
@@ -1,6 +0,0 @@
-# def test():
-#     first_file = r'../../tests/fixtures/json/file3.json'
-#     second_file = r'../../tests/fixtures/json/file4.json'
-#     print(open_files(first_file, second_file))
-#
-# test()
